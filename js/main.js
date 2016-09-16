eff268932f5aa76eb005a901a8fae7b3a235e171
ANDREA <andrea.palomino.ruby@gmail.com>
2016-09-16 11:46:24 -0700

    moificado

--- js/main.js
+++ js/main.js
@@ -1,85 +1,98 @@
 function validateForm(){
-	/* Escribe tú código aquí */
-var name = document.getElementById("name").value;
-var lastname = document.getElementById("lastname").value;
-var email = document.getElementById("input-email").value;
-var inputPassword = document.getElementById("input-password").value;
-var formControl = document.querySelector("select.form-control").value;
-var contra = document.getElementById("input-password").value;
-var validacion = document.createElement("span");
-var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-var expreg = /^[a-z][a-z]*/;
+	var name = document.getElementById("name").value;
+    var lastname = document.getElementById("lastname").value;
+    var inputEmail = document.getElementById("input-email").value;
+    var inputPassword = document.getElementById("input-password").value;
+    var contra = document.getElementById("input-password").value;
 
-  if(!name.match(expreg)) {
-    inlineMsg('name','You have entered an invalid name.',2);
-    return false;
-  }
-  if(email == "") {
-    inlineMsg('email','<strong>Error</strong><br />You must enter your email.',2);
-    return false;
-  }
-  if(!email.match(emailRegex)) {
-    inlineMsg('email','<strong>Error</strong><br />You have entered an invalid email.',2);
-    return false;
-  }
-  if(inputPassword == "") {
-    inlineMsg('gender','<strong>Error</strong><br />You must select your gender.',2);
-    return false;
-  }
-  if(contra == "") {
-    inlineMsg('message','You must enter a message.');
-    return false;
-  }
-  if(!lastname.match(expr)) {
-    inlineMsg('name','You have entered an invalid name.',2);
-    return false;
-  }
+    var cuadroDeValidacion = document.createElement("span");
+    var cuadroDeValidacion2 = document.createElement("span");
+    var cuadroDeValidacion3 = document.createElement("span");
+    var cuadroDeValidacion4 = document.createElement("span");
+    var cuadroDeValidacion5 = document.createElement("span");
 
-  function inlineMsg(target,string,autohide) {
-  var msg;
-  var msgcontent;
-  if(!document.getElementById('msg')) {
-    msg = document.createElement('div');
-    msg.id = 'msg';
-    msgcontent = document.createElement('div');
-    msgcontent.id = 'msgcontent';
-    document.body.appendChild(msg);
-    msg.appendChild(msgcontent);
-    msg.style.filter = 'alpha(opacity=0)';
-    msg.style.opacity = 0;
-    msg.alpha = 0;
-  } else {
-    msg = document.getElementById('msg');
-    msgcontent = document.getElementById('msgcontent');
-  }
-  msgcontent.innerHTML = string;
-  msg.style.display = 'block';
-  var msgheight = msg.offsetHeight;
-  var targetdiv = document.getElementById(target);
-  targetdiv.focus();
-  var targetheight = targetdiv.offsetHeight;
-  var targetwidth = targetdiv.offsetWidth;
-  var topposition = topPosition(targetdiv) - ((msgheight - targetheight) / 2);
-  var leftposition = leftPosition(targetdiv) + targetwidth + MSGOFFSET;
-  msg.style.top = topposition + 'px';
-  msg.style.left = leftposition + 'px';
-  clearInterval(msg.timer);
-  msg.timer = setInterval("fadeMsg(1)", MSGTIMER);
-  if(!autohide) {
-    autohide = MSGHIDE;  
-  }
-  window.setTimeout("hideMsg()", (autohide * 1000));
+    //Email
+    var expr = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (expr.test(inputEmail) ){
+    	var cuadroNegro = document.getElementsByClassName("input-box")[2];
+    	verificar(cuadroNegro.getElementsByTagName("span")[0], cuadroNegro);
+    }
+
+    else if(inputEmail == ""){
+    	var cuadroNegro = document.getElementsByClassName("input-box")[2];
+    	cuadroNegro.appendChild(cuadroDeValidacion);
+    	cuadroDeValidacion.innerHTML= "Verifique su E-mail";
+    	cuadroNegro.classList.toggle("error");
+    }
+        
+    else{
+    	var cuadroNegro = document.getElementsByClassName("input-box")[2];
+    	cuadroNegro.appendChild(cuadroDeValidacion);
+        cuadroDeValidacion.innerHTML= "Verifique su E-mail";
+        cuadroNegro.classList.add("error");
+    }
+
+    //Seleccionar una opción
+
+    if( document.querySelector("select").value == 0 ){
+    	var cuadroNegro = document.getElementsByClassName("input-box")[4];
+   		cuadroNegro.appendChild(cuadroDeValidacion2);
+        cuadroDeValidacion2.innerHTML= "Ingrese una opción";
+        cuadroNegro.classList.add("error");
+    } else {
+    	var cuadroNegro = document.getElementsByClassName("input-box")[4];
+    	verificar(cuadroNegro.getElementsByTagName("span")[0], cuadroNegro);
+    }
+
+    //Contraseña
+    if (contra.length< 6 || (contra == "password") || (contra == "123456") || (contra=="098754")){
+    	var cuadroNegro = document.getElementsByClassName("input-box")[3];
+   		cuadroNegro.appendChild(cuadroDeValidacion3);
+        cuadroDeValidacion3.innerHTML= "La contraseña tiene que tener al menos 6 caracteres";
+        cuadroNegro.classList.add("error");
+    }else{
+    	var cuadroNegro = document.getElementsByClassName("input-box")[3];
+    	verificar(cuadroNegro.getElementsByTagName("span")[0], cuadroNegro);
+    }
+
+    //Para los campos nombre y apellido la primera letra debe ser mayúscula
+    //name 
+    if (name.charAt(0) == name.charAt(0).toLowerCase() && name.lenght >0){
+        var cuadroNegro = document.getElementsByClassName("input-box")[0];
+   		cuadroNegro.appendChild(cuadroDeValidacion4);
+        cuadroDeValidacion4.innerHTML= "La primera letra es con mayúscula.";
+        cuadroNegro.classList.add("error");
+    }else if (name == ""){
+        var cuadroNegro = document.getElementsByClassName("input-box")[0];
+   		cuadroNegro.appendChild(cuadroDeValidacion4);
+        cuadroDeValidacion4.innerHTML= "Debe ingresar su nombre.";
+        cuadroNegro.classList.add("error");
+    }else{
+    	var cuadroNegro = document.getElementsByClassName("input-box")[0];
+    	verificar(cuadroNegro.getElementsByTagName("span")[0], cuadroNegro);
+    }
+    
+    //lastname
+    if (lastname.charAt(0) == lastname.charAt(0).toLowerCase()){
+        var cuadroNegro = document.getElementsByClassName("input-box")[1];
+   		cuadroNegro.appendChild(cuadroDeValidacion5);
+        cuadroDeValidacion5.innerHTML= "La primera letra es con mayúscula.";
+        cuadroNegro.classList.add("error");
+   	}else if(lastname == ""){
+        var cuadroNegro = document.getElementsByClassName("input-box")[1];
+   		cuadroNegro.appendChild(cuadroDeValidacion5);
+        cuadroDeValidacion5.innerHTML= "Debe ingresar su nombre.";
+        cuadroNegro.classList.add("error");
+    }else{
+    	var cuadroNegro = document.getElementsByClassName("input-box")[1];
+    	verificar(cuadroNegro.getElementsByTagName("span")[0], cuadroNegro);
+    }
 }
 
-
-
-
-
-	    //Seleccionar una opción
-
-		
-	   
-
-
-
- 
+function verificar(a,b){
+	if( a!= null)
+        b.removeChild(a);
+    else
+    	true;
+}
